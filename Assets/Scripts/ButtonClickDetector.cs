8b3370e60ae1426254798525f17a58b6973a3636
Riley-Pillar Mac <rmoss@pillartechnology.com>
2019-05-21 15:02:56 -0500

    First commit, camera rotation and location tracking working

---
+++ Assets/Scripts/ButtonClickDetector.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+// Attach this script to the camera that you want to follow the target
+public class ButtonClickDetector : MonoBehaviour {
+
+	public Button syncButton;
+	public Transform syncPoint;
+
+
+
+	void OnDisable()
+	{
+		syncButton.onClick.RemoveAllListeners();
+	}
+}
